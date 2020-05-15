24490c8c854ebfbd502073c320be6169b09bd535
vauxhall <zero.griffin@gmail.com>
2020-05-15 15:54:41 +0200

    Add more tests

--- Sources/Safer.swift
+++ Sources/Safer.swift
@@ -6,6 +6,10 @@ public final class Safer {
     }
     
     public func analyse(_ cookies: [HTTPCookie]) -> Report {
-        .init(rating: .good, cookies: 0)
+        switch cookies.count {
+        case let count where count < 1: return .init(rating: .good, cookies: count)
+        case let count where count > 0 && count < 4: return .init(rating: .ok, cookies: count)
+        default: return .init(rating: .bad, cookies: cookies.count)
+        }
     }
 }
--- Tests/CookiesTests.swift
+++ Tests/CookiesTests.swift
@@ -12,4 +12,30 @@ final class CookiesTests: XCTestCase {
         XCTAssertEqual(.good, safer.analyse([]).rating)
         XCTAssertEqual(0, safer.analyse([]).cookies)
     }
+    
+    func testOkCookies() {
+        XCTAssertEqual(.ok, safer.analyse([
+            HTTPCookie(properties: [.name: "a", .domain: "avoca.de", .path: "/", .value: "a"])!,
+            HTTPCookie(properties: [.name: "b", .domain: "avoca.de", .path: "/", .value: "b"])!,
+            HTTPCookie(properties: [.name: "c", .domain: "avoca.de", .path: "/", .value: "c"])!]).rating)
+        XCTAssertEqual(3, safer.analyse([
+            HTTPCookie(properties: [.name: "a", .domain: "avoca.de", .path: "/", .value: "a"])!,
+            HTTPCookie(properties: [.name: "b", .domain: "avoca.de", .path: "/", .value: "b"])!,
+            HTTPCookie(properties: [.name: "c", .domain: "avoca.de", .path: "/", .value: "c"])!]).cookies)
+    }
+    
+    func testBadCookies() {
+        XCTAssertEqual(.bad, safer.analyse([
+            HTTPCookie(properties: [.name: "a", .domain: "avoca.de", .path: "/", .value: "a"])!,
+            HTTPCookie(properties: [.name: "b", .domain: "avoca.de", .path: "/", .value: "b"])!,
+            HTTPCookie(properties: [.name: "c", .domain: "avoca.de", .path: "/", .value: "c"])!,
+            HTTPCookie(properties: [.name: "d", .domain: "avoca.de", .path: "/", .value: "d"])!,
+            HTTPCookie(properties: [.name: "e", .domain: "avoca.de", .path: "/", .value: "e"])!]).rating)
+        XCTAssertEqual(5, safer.analyse([
+            HTTPCookie(properties: [.name: "a", .domain: "avoca.de", .path: "/", .value: "a"])!,
+            HTTPCookie(properties: [.name: "b", .domain: "avoca.de", .path: "/", .value: "b"])!,
+            HTTPCookie(properties: [.name: "c", .domain: "avoca.de", .path: "/", .value: "c"])!,
+            HTTPCookie(properties: [.name: "d", .domain: "avoca.de", .path: "/", .value: "d"])!,
+            HTTPCookie(properties: [.name: "e", .domain: "avoca.de", .path: "/", .value: "e"])!]).cookies)
+    }
 }
